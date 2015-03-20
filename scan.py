fab4ae06a664f65da35181382c46154544cd4c59
Aleksandar Gyorev <alexander.gyorev@gmail.com>
2015-03-20 20:12:53 +0100

    + changed blurring strategy, and now the contour is being approximated using its convex hull, since we always expect rectangles

--- scan.py
+++ scan.py
@@ -20,8 +20,8 @@ ap.add_argument('-i',
 ap.add_argument('-H',
     '--height',
     required = False,
-    default  = 500,
-    help     = 'height of the image image we will process and use for finding the contours (default: 500)')
+    default  = 300,
+    help     = 'height of the image image we will process and use for finding the contours (default: 300)')
 
 ap.add_argument('-n',
     '--noise',
@@ -35,23 +35,28 @@ HEIGHT              = int(args['height'])
 NOISE_REMOVAL_LEVEL = max(int(args['noise']) * 2 - 1, 0)
 bi                  = BasicImage(args['image'])
 
-original = bi.get().copy()
-ratio    = original.shape[0] / float(HEIGHT)
-image    = bi.resize('H', HEIGHT)
+original   = bi.get().copy()
+ratio      = original.shape[0] / float(HEIGHT)
+image      = bi.resize('H', HEIGHT)
+total_area = image.shape[0] * image.shape[1]
 
 """ Step 1: Edge Detection """
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # get the grayscale image
-gray = cv2.GaussianBlur(gray, (3, 3), 0) # with a bit of blurring
-#BasicImage(gray).show()
+gray = cv2.bilateralFilter(gray, 11, 17, 17)
+#gray = cv2.GaussianBlur(gray, (3, 3), 0) # with a bit of blurring
+BasicImage(gray).show()
 
-edged = cv2.Canny(gray, 10, 250) # detect edges (outlines) of the objects
-#BasicImage(edged).show()
+high_thresh, thresh_im = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+low_thresh = high_thresh / 2.0
+
+edged = cv2.Canny(gray, low_thresh, high_thresh) # detect edges (outlines) of the objects
+BasicImage(edged).show()
 
 # since some of the outlines are not exactly clear, we construct
 # and apply a closing kernel to close the gaps b/w white pixels
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
 closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
-#BasicImage(closed).show()
+BasicImage(closed).show()
 
 """ Step 2: Finding Contours """
 (contours, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -61,8 +66,14 @@ total = 0
 approx_all = []
 for contour in contours:
     # approximating the contour
-    peri   = cv2.arcLength(contour, True)
-    approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
+    contour = cv2.convexHull(contour)
+    peri    = cv2.arcLength(contour, True)
+    approx  = cv2.approxPolyDP(contour, 0.02 * peri, True)
+    area    = cv2.contourArea(contour)
+
+    # we don't consider anything less than 5% of the whole image
+    if area < 0.05 * total_area:
+        continue
 
     # if the approximated contour has 4 points, then assumer it is a book
     # a book is a rectangle and thus it has 4 vertices
@@ -77,6 +88,7 @@ BasicImage(image).show()
 """ Step 3: Apply a Perspective Transform and Threshold """
 for approx in approx_all:
     warped = Transform.get_box_transform(original, approx.reshape(4, 2) * ratio)
+    BasicImage(warped).show()
 
     warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
     warped = cv2.medianBlur(warped, NOISE_REMOVAL_LEVEL)
