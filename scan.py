89d7bf771c4cbd81a46037bf8478b0e95832176e
Aleksandar Gyorev <alexander.gyorev@gmail.com>
2015-05-14 21:14:03 +0200

    - added an option for auto heuristical estimation of the height and closing   parameters for finding the objects

--- scan.py
+++ scan.py
@@ -36,6 +36,13 @@ ap.add_argument('-c',
     default  = 3,
     help     = 'the size of the closing element after applying the Canny edge detector')
 
+ap.add_argument('-a',
+    '--auto',
+    required = False,
+    action   = 'store_true',
+    default  = False,
+    help     = 'if we want to have automatically set values for the height and closing when looking for objects')
+
 ap.add_argument('-s',
     '--save',
     action   = 'store_true',
@@ -49,89 +56,116 @@ NOISE_REMOVAL_LEVEL = max(int(args['noise']) * 2 - 1, 0)
 CLOSING_SIZE        = int(args['closing'])
 bi                  = BasicImage(args['image'])
 
-original   = bi.get().copy()
-ratio      = original.shape[0] / float(HEIGHT)
-image      = bi.resize('H', HEIGHT)
-total_area = image.shape[0] * image.shape[1]
+def scan():
+    """ Step 1: Edge Detection """
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # get the grayscale image
+    gray = cv2.bilateralFilter(gray, 11, 17, 17)
+    #gray = cv2.GaussianBlur(gray, (3, 3), 0) # with a bit of blurring
+    #BasicImage(gray).show()
 
-#BasicImage(image).show()
+    # automatic Canny edge detection thredhold computation
+    high_thresh, thresh_im = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    low_thresh = high_thresh / 2.0
 
-""" Step 1: Edge Detection """
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # get the grayscale image
-gray = cv2.bilateralFilter(gray, 11, 17, 17)
-#gray = cv2.GaussianBlur(gray, (3, 3), 0) # with a bit of blurring
-#BasicImage(gray).show()
+    # zero-parameter automatic Canny edge detection (method 2)
+    # Vary the percentage thresholds that are determined (in practice 0.33 tends to give good approx. results)
+    # A lower value of sigma  indicates a tighter threshold, whereas a larger value of sigma  gives a wider threshold.
+    #sigma       = 0.33
+    #v           = np.median(gray)
+    #low_thresh  = int(max(0, (1.0 - sigma) * v))
+    #high_thresh = int(min(255, (1.0 + sigma) * v))
 
-# automatic Canny edge detection thredhold computation
-high_thresh, thresh_im = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-low_thresh = high_thresh / 2.0
+    edged = cv2.Canny(gray, low_thresh, high_thresh) # detect edges (outlines) of the objects
+    #BasicImage(edged).show()
 
-# zero-parameter automatic Canny edge detection (method 2)
-# Vary the percentage thresholds that are determined (in practice 0.33 tends to give good approx. results)
-# A lower value of sigma  indicates a tighter threshold, whereas a larger value of sigma  gives a wider threshold.
-#sigma       = 0.33
-#v           = np.median(gray)
-#low_thresh  = int(max(0, (1.0 - sigma) * v))
-#high_thresh = int(min(255, (1.0 + sigma) * v))
+    # since some of the outlines are not exactly clear, we construct
+    # and apply a closing kernel to close the gaps b/w white pixels
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (CLOSING_SIZE, CLOSING_SIZE))
+    closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+    #BasicImage(closed).show()
 
-edged = cv2.Canny(gray, low_thresh, high_thresh) # detect edges (outlines) of the objects
-#BasicImage(edged).show()
+    """ Step 2: Finding Contours """
+    (contours, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    total = 0
 
-# since some of the outlines are not exactly clear, we construct
-# and apply a closing kernel to close the gaps b/w white pixels
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (CLOSING_SIZE, CLOSING_SIZE))
-closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
-#BasicImage(closed).show()
+    # looping over the contours found
+    approx_all = []
+    for contour in contours:
+        # approximating the contour
+        contour = cv2.convexHull(contour)
+        peri    = cv2.arcLength(contour, True)
+        approx  = cv2.approxPolyDP(contour, 0.02 * peri, True)
+        area    = cv2.contourArea(contour)
 
-""" Step 2: Finding Contours """
-(contours, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-total = 0
+        # we don't consider anything less than 5% of the whole image
+        if area < 0.05 * total_area:
+            continue
 
-# looping over the contours found
-approx_all = []
-for contour in contours:
-    # approximating the contour
-    contour = cv2.convexHull(contour)
-    peri    = cv2.arcLength(contour, True)
-    approx  = cv2.approxPolyDP(contour, 0.02 * peri, True)
-    area    = cv2.contourArea(contour)
+        # if the approximated contour has 4 points, then assumer it is a book
+        # a book is a rectangle and thus it has 4 vertices
+        if len(approx) == 4:
+            cv2.drawContours(image, [approx], -1, (0, 255, 0), 4)
+            approx_all.append(approx)
+            total += 1
 
-    # we don't consider anything less than 5% of the whole image
-    if area < 0.05 * total_area:
-        continue
+    print 'Found %d books/papers in the image.' % total
+    #BasicImage(image).show()
 
-    # if the approximated contour has 4 points, then assumer it is a book
-    # a book is a rectangle and thus it has 4 vertices
-    if len(approx) == 4:
-        cv2.drawContours(image, [approx], -1, (0, 255, 0), 4)
-        approx_all.append(approx)
-        total += 1
+    # no point of displaying anything if we couldn't find any books
+    if total != 0:
+        """ Displaying all intermediate steps into one image """
+        top_row = CombineImages(300, original, gray)
+        bot_row = CombineImages(300, closed, image)
+        BasicImage(top_row).show()
+        BasicImage(bot_row).show()
+        #com_img = np.vstack((top_row, bot_row))
+        #BasicImage(com_img).show()
 
-print 'Found %d books/papers in the image.' % total
-#BasicImage(image).show()
+        """ Step 3: Apply a Perspective Transform and Threshold """
+        total = 0
+        for approx in approx_all:
+            total += 1
+            warped = Transform.get_box_transform(original, approx.reshape(4, 2) * ratio)
+            #BasicImage(warped).show()
 
-""" Displaying all intermediate steps into one image """
-top_row = CombineImages(300, original, gray)
-bot_row = CombineImages(300, closed, image)
-com_img = np.vstack((top_row, bot_row))
-BasicImage(com_img).show()
+            scan_warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+            scan_warped = cv2.medianBlur(scan_warped, NOISE_REMOVAL_LEVEL)
+            scan_warped = cv2.adaptiveThreshold(scan_warped, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+            #BasicImage(scan_warped).show()
+            BasicImage(CombineImages(400, warped, scan_warped)).show()
 
-""" Step 3: Apply a Perspective Transform and Threshold """
-total = 0
-for approx in approx_all:
-    total += 1
-    warped = Transform.get_box_transform(original, approx.reshape(4, 2) * ratio)
-    #BasicImage(warped).show()
+            # save the image
+            if args['save'] == True:
+                filename_color = 'scan%03d_color.jpg' % total
+                filename_scan  = 'scan%03d_scan.jpg' % total
+                BasicImage(warped).save(filename_color)
+                BasicImage(scan_warped).save(filename_scan)
 
-    scan_warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-    scan_warped = cv2.medianBlur(scan_warped, NOISE_REMOVAL_LEVEL)
-    scan_warped = cv2.adaptiveThreshold(scan_warped, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    #BasicImage(scan_warped).show()
-    BasicImage(CombineImages(400, warped, scan_warped)).show()
+    return total
 
-    # save the image
-    if args['save'] == True:
-        filename_color = 'scan%03d_color.jpg' % total
-        filename_scan  = 'scan%03d_scan.jpg' % total
-        BasicImage(warped).save(filename_color)
-        BasicImage(scan_warped).save(filename_scan)
+if args['auto'] == False:
+    original   = bi.get().copy()
+    ratio      = original.shape[0] / float(HEIGHT)
+    image      = bi.resize('H', HEIGHT)
+    total_area = image.shape[0] * image.shape[1]
+
+    #BasicImage(image).show()
+
+    scan()
+else:
+    print range(650, 300, -50)
+    for auto_height in xrange(min(650, bi.get().shape[0]), 299, -50):
+        for auto_closing in xrange(6, 1, -1):
+            HEIGHT       = auto_height
+            CLOSING_SIZE = auto_closing
+
+            original     = bi.get().copy()
+            ratio        = original.shape[0] / float(HEIGHT)
+            image        = bi.resize('H', HEIGHT)
+            total_area   = image.shape[0] * image.shape[1]
+
+            print 'auto_height = ', auto_height
+            print 'auto_closing= ', auto_closing
+            if scan() != 0:
+                exit(0)
+
